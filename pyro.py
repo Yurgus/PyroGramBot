192ce3e998461cfec3bf97d65be3a6e0810aaa53
Yurgus <yugus@mail.ru>
2020-05-04 20:17:58 +0300

    Update pyro.py

--- pyro.py
+++ pyro.py
@@ -3,5 +3,5 @@ from pyrogram import Client
 api_id = 1364592
 api_hash = "bacb68050efb668d82dd9d1039759f49"
 
-with Client("yurgus", api_id, api_hash) as app:
+with Client("Universal_shat_bot", api_id, api_hash) as app:
     app.send_message("me", "Greetings from **Pyrogram**!")
